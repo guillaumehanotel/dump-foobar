4dc6a1b0e5b5d0ae7574f48a062a4c07a65a6306
Guillaume HANOTEL <guillaumehanotel@orange.fr>
2020-02-27 18:28:20 +0100

    add declaration

--- dist/main.d.ts
+++ dist/main.d.ts
@@ -8,6 +8,8 @@ export * from './orm/schemaBuilder';
 export * from './orm/exceptions';
 export { default as Controller } from './controller';
 export { default as Router } from './router';
+export { default as Foobar } from './foobar';
+export { default as Model } from './orm/model';
 export { default as Column } from './orm/decorators/Column';
 export { default as BelongsTo } from './orm/decorators/BelongsTo';
 export { default as HasMany } from './orm/decorators/HasMany';
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,16 @@
 export * from './controller'
 export * from './router'
 export * from './exceptions'
-
 export * from './foobar'
 export * from './orm/model'
-
 export * from './orm/decorators'
 export * from './orm/schemaBuilder'
 export * from './orm/exceptions'
 
 export { default as Controller } from './controller'
 export { default as Router } from './router'
+export { default as Foobar } from './foobar'
+export { default as Model } from './orm/model'
 export { default as Column } from './orm/decorators/Column'
 export { default as BelongsTo } from './orm/decorators/BelongsTo'
 export { default as HasMany } from './orm/decorators/HasMany'
